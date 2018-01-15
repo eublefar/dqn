e9d19bd2a82cc081b579b61cc44815c4b6b21402
eublefar <evil.unicorn1@gmail.com>
2018-01-15 09:12:12 +0100

    Squashed multiple commits with finishing main loop

--- dqn.py
+++ dqn.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import util
 
 class DQN:
 
@@ -15,12 +16,13 @@ class DQN:
 
         with tf.variable_scope('DQN_%d'%(self.id,)):
             self.batch_size = batch_size
-            batch_shape = [batch_size,] + list(observation_space.shape)
-            self.input = tf.placeholder(dtype=tf.float32, shape=batch_shape)
+            batch_shape = [None,] + list(observation_space.shape)
+            self.input = tf.placeholder(dtype=tf.float32, shape=batch_shape, name="input")
             outp = self._defineModel(self.input)
             self.Q = tf.layers.dense(inputs=outp, units=action_space.n,
                                         activation=None, name='output')
-            self.targetQ = tf.placeholder(shape=[None], dtype=tf.float32)
+            self.targetQ = tf.placeholder(shape=[None], dtype=tf.float32, name="targetQ_ph")
+            # TODO: dobavit onehot vektor dla dejstvij
             self.loss = tf.reduce_mean(tf.square(self.targetQ - self.Q))
             self._trainer = tf.train.AdamOptimizer(learning_rate=0.0001)
             self.updateModel = self._trainer.minimize(self.loss)
@@ -28,26 +30,29 @@ class DQN:
     def applyUpdate(self, source, tau):
         src_vars = tf.trainable_variables(scope="DQN_%d"%(source.id))
         target_vars = tf.trainable_variables(scope="DQN_%d"%(self.id))
+        update_ops = []
         for i in range(len(src_vars)):
-            var = target_vars.find(
-            lambda x:
-            x.name[len('DQN_#/'):] == src_vars[i].name[len('DQN_#/'):]
-            )
-            var.assign(var.value() * (1 - tau) + src_vars[i] * tau)
+            var = util.map_dqn_var(src_vars[i], target_vars)
+            op = var.assign(var.value() * (1 - tau) + src_vars[i] * tau)
+            update_ops.append(op)
+        return update_ops
 
     def _defineModel(self, inp):
-        inp = tf.reshape(tensor=inp, shape=inp.get_shape().as_list()+[1,])
+        # inp = tf.reshape(tensor=inp, shape=inp.get_shape().as_list()+[1,])
         conv1 = tf.layers.conv2d(inputs=inp, kernel_size=[1,1],
-                                filters=64, activation=tf.nn.relu, name='conv1')
+                                filters=32, activation=tf.nn.relu, name='conv1')
         conv2 = tf.layers.conv2d(inputs=conv1, kernel_size=[1,1],
                                 filters=32, activation=tf.nn.relu, name='conv2')
         conv3 = tf.layers.conv2d(inputs=conv2, kernel_size=[1,5],
                                 filters=16, activation=tf.nn.relu, name='conv3')
-        conv4 = tf.layers.conv2d(inputs=conv3, kernel_size=[7,1],
+        max_pool = tf.layers.max_pooling2d(conv3, [3,1], [3,1], 'same')
+        conv4 = tf.layers.conv2d(inputs=max_pool, kernel_size=[7,1],
                                 filters=16, activation=tf.nn.relu, name='conv4')
-        dense1 = tf.layers.dense(inputs=conv4, units=512,
+        max_pool1 = tf.layers.max_pooling2d(conv4, [1,3], [1,3], 'same')
+        flat = tf.layers.flatten(max_pool1)
+        dense1 = tf.layers.dense(inputs=flat, units=512,
                                 activation=tf.nn.relu, name='dense1')
-        return _dense1
+        return dense1
 
     def _defineModel_noConv(self, inp):
         dense1 = tf.layers.dense(inputs=inp, units=512,
@@ -57,16 +62,28 @@ class DQN:
         return dense2
 
 class experience_buffer:
-    def __init__(self, buffer_size=10000):
+    def __init__(self, buffer_size=100000):
         self.buffer = []
         self.buffer_size = buffer_size
         self.iter = iter(self.buffer)
         self.i=0
 
     def add(self, experience):
-        if len(self.buffer) + len(experience) >= self.buffer_size:
-            self.buffer[0:(len(experience)+len(self.buffer))-self.buffer_size] = []
-        self.buffer.extend(experience)
+        if np.array(experience).ndim == 1:
+            if len(self.buffer) + 1 >= self.buffer_size:
+                self.buffer[0:1] = []
+            self.buffer.append(experience)
+        else:
+            if len(self.buffer) + len(experience) >= self.buffer_size:
+                self.buffer[0:(len(experience)+len(self.buffer))-self.buffer_size] = []
+            self.buffer.extend(experience)
 
     def sample(self,size):
-        return np.reshape(np.array(random.sample(self.buffer,size)),[size,5])
+        try:
+            if size <= len(self.buffer):
+                return np.reshape(np.array(np.random.permutation(self.buffer)[0:size]),[size,4])
+            else:
+                return np.reshape(np.array(np.random.permutation(self.buffer)[0:size]),[-1,4])
+        except ValueError:
+            print(size <= len(self.buffer))
+            raise
--- train.py
+++ train.py
@@ -2,18 +2,96 @@ from gym import spaces
 import gym
 import tensorflow as tf
 import dqn
+import numpy as np
 
-env = gym.make('MountainCar-v0')
-batch_size=5
-tau = 0.001
-env.observation_space
-target = dqn.DQN(env.observation_space, env.action_space, batch_size=batch_size)
-main = dqn.DQN(env.observation_space, env.action_space, batch_size=batch_size)
-exp_buffer = dqn.experience_buffer()
-obs = env.reset()
-for i in range(batch_size):
-    exp_buffer.add(env.step(env.action_space.sample()))
 
-exp_buffer.buffer
+if __name__=="__main__":
 
-for x in xrange(10000):
+    env = gym.make('SpaceInvaders-v0')
+
+    restore_model = False
+    restore_dir = 'checkpoints/checkpoint_0.ckpt'
+    num_episodes = 10000
+    num_pretrain_steps = 10000
+    num_steps_per_episode = 100000
+    eps_high = 1.
+    eps_low = 0.1
+    eps_degrade_steps = 10000
+    eps = eps_high
+    eps_step = (eps_high - eps_low)/eps_degrade_steps
+    checkpoint_episode_num = 50
+    batch_size=10
+    tau = 0.001
+    discound = 0.8
+
+    target = dqn.DQN(env.observation_space, env.action_space,
+                    batch_size=batch_size, pixels = True)
+    main = dqn.DQN(env.observation_space, env.action_space,
+                    batch_size=batch_size, pixels = True)
+
+    apply_update_op = target.applyUpdate(main, tau)
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    writer = tf.summary.FileWriter('/tmp/dqn/1')
+
+    num_steps = 0
+# Reproductableness for the win
+    seed = 7
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+    env.seed(seed)
+
+    with tf.Session() as session:
+        session.run(init)
+        if restore_model:
+            ckpt = tf.train.get_checkpoint_state(restore_dir)
+            saver.restore(session,ckpt.model_checkpoint_path)
+
+        writer.add_graph(session.graph)
+        for var in tf.global_variables():
+            tf.summary.scalar(var.name, var)
+        summary_op = tf.summary.merge_all()
+
+        for episode in range(num_episodes):
+            exp_buffer = dqn.experience_buffer()
+            obs = env.reset()
+            ep_steps = 0
+            done = False
+            if episode%checkpoint_episode_num == 0:
+                saver.save(session, restore_dir)
+            while not done:
+                num_steps+=1
+                ep_steps+=1
+
+                if num_steps <= num_pretrain_steps or np.random.random_sample() < eps:
+                    action = env.action_space.sample()
+                else:
+                    action = np.argmax(session.run(main.Q, feed_dict={main.input:obs}))
+
+                obs_next, reward, done, _ = env.step(action)
+                exp_buffer.add((obs,action,reward,obs_next))
+
+                if num_steps >= num_pretrain_steps:
+                    try:
+                        batch = exp_buffer.sample(batch_size)
+                        next_Q = session.run(target.Q, feed_dict={target.input:np.stack(batch[:,3])})
+                        targetQ = batch[:,2] + discound*np.max(next_Q, 1)
+                        session.run(apply_update_op)
+
+                        session.run(main.updateModel, feed_dict={main.input:np.stack(batch[:,0]), main.targetQ: targetQ})
+                        print(tarassas)
+                        eps -= eps_step
+                    except ValueError:
+                        print (num_steps, ep_steps, batch[:,3])
+                        print(target.Q.get_shape().as_list(), batch[:,2])
+                        raise
+
+                obs = obs_next
+
+                if ep_steps >= num_steps_per_episode:
+                    done = True
+                # summary = session.run(summary_op)
+                # writer.add_summary(summary, num_steps)
+                # env.render()
+                # TODO: Dodelat' summary, poreshat' s pretraining steps i degradaciej epsilon
